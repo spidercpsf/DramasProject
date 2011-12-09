abe6f70c85e370aee7d21f972a8ebfad74e68bd4
wataridori <thangtd90@gmail.com>
2011-12-09 14:27:01 +0900

    Create project

---
+++ drama/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drama::Application.config.secret_token = 'c6edf7cf32d7338889a5400668512bda10e7c419143996ba62f5ed51d95b077a609f92c7a7822f2c6eb0f6616583f30e3d2aad69f389030959eb07e4d3660523'
