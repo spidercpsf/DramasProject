5a786ae1418817c4b077e7114cbd10a90880d0a8
Minh Giang <spider@ht.sfc.keio.ac.jp>
2012-01-03 17:49:20 +0900

    most view, like, comment. using rake db:migarate to change database

--- dp/db/schema.rb
+++ dp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120101060211) do
+ActiveRecord::Schema.define(:version => 20120103083134) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(:version => 20120101060211) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "like"
+    t.integer  "view"
   end
 
   create_table "genres", :force => true do |t|
