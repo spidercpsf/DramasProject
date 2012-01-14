fd0f51bc028fac84ab410d89fe8d59bb27e94ba6
wataridori <thangtd90@gmail.com>
2012-01-15 00:35:34 +0900

    thang

--- dp/app/controllers/home_controller.rb
+++ dp/app/controllers/home_controller.rb
@@ -10,15 +10,17 @@ class HomeController < ApplicationController
       @dramas_sort_like = Drama.find(:all).sort_by{|p| -p.like_num}
       @dramas_sort_view = Drama.find(:all).sort_by{|p| -p.view}
       @dramas_sort_comment = Drama.find(:all).sort_by{|p| -p.comment.length}
-      @allcom = Array.new
-      current_user.drama.each do |d|
+      if user_signed_in? then 
+        @allcom = Array.new
+        current_user.drama.each do |d|
         com = d.comment  
-        l = com.length
-        if l > 0
-          @allcom = @allcom << com[l-1]
-        end 
-       end  
-       @allcom = @allcom.sort{|a,b| b.created_at <=> a.created_at }
+          l = com.length
+          if l > 0
+            @allcom = @allcom << com[l-1]
+          end 
+        end  
+        @allcom = @allcom.sort{|a,b| b.created_at <=> a.created_at }
+      end
   end
 
 end
